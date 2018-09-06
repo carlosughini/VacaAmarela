3349805d84d0f90b3be5c7e17f343c98fe6bb57b
Carlos Henrique Ughini <carlos.ughini@gmail.com>
2018-09-05 22:26:23 -0300

    Created the four fragments that will appear in the DetailActivity

---
+++ app/src/main/java/com/vacaamarela/carlos/vacaamarela/ButcheryDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.vacaamarela.carlos.vacaamarela
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ButcheryDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_butchery_detail)
+    }
+}
